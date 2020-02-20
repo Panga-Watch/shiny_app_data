92159ca8a3d509eba51c8feb67563274efb0a1b5
Corinna <corinna@ucsb.edu>
2020-02-20 13:20:50 -0800

    Actual app!

---
+++ app.R
@@ -0,0 +1,16 @@
+# Panga ShinyApp
+
+
+# Load packages
+library(shiny)
+library(tidyverse)
+library(shinythemes)
+
+ui <- navbarPage()
+
+
+
+server <- function(input, output) {}
+
+
+shinyApp(ui = ui, server = server)
